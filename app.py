78fe03dd59a983c4c4149c81596efb4e77a401a5
vic.betvncourt@gmail.com <vic.betvncourt@gmail.com>
2022-12-12 20:44:44 -0600

    testing webhook

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/git_update', methods="POST")
 def git_update():
-    repo = git.repo(./vic_cs50_final)
+    repo = git.repo(/vic_cs50_final)
     origin = repo.remotes.origin
     repo.create_head('main',
     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
